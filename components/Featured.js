c5c394ddd2bc6f7f8374bd9178d1c39f0a4c102d
Jcvalencia05 <valencia.jc93@gmail.com>
2024-09-23 12:55:18 -0500

    header project

---
+++ components/Featured.js
@@ -0,0 +1,10 @@
+
+
+export default function Featured() {
+return(
+    <div>
+        <h1>Prueba de texto</h1>
+        <p>jajajajajaajajajajajjjajajajjajajajajajaja</p>
+    </div>
+);
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,31 @@
-import '@/styles/globals.css'
+import { createGlobalStyle } from "styled-components";
 
+const GlobalStyles = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Poppins:ital@0;1&display=swap');
+    body{
+      padding: 0;
+      margin:0;
+      .poppins-regular {
+        font-family: "Poppins", sans-serif;
+        font-weight: 400;
+        font-style: normal;
+    }
+
+      .poppins-regular-italic {
+        font-family: "Poppins", sans-serif;
+        font-weight: 400;
+        font-style: italic;
+}
+
+    }
+`;
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />
+
+    </>
+
+  );
 }
